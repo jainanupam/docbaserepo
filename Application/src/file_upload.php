567a7444594af7f9a32edcf0e3ab005376f368bc
Anupam Jain <anupam2204.smart@gmail.com>
2013-07-05 18:12:44 +0530

    Added basic file upload functionality

---
+++ Application/src/file_upload.php
@@ -0,0 +1,35 @@
+<?php
+
+// Check for errors
+if($_FILES['file_upload']['error'] > 0){
+    die('An error ocurred when uploading. '.$_FILES['file_upload']['error']);
+}
+
+if(!getimagesize($_FILES['file_upload']['tmp_name'])){
+    die('Please ensure you are uploading an image.');
+}
+
+// Check filetype
+if($_FILES['file_upload']['type'] != 'image/jpeg'){
+    die('Unsupported filetype uploaded.');
+}
+
+// Check filesize
+if($_FILES['file_upload']['size'] > 3000000){
+    die('File uploaded exceeds maximum upload size.');
+}
+
+// Check if the file exists
+if(file_exists('../../upload/' . $_FILES['file_upload']['name'])){
+    die('File with that name already exists.');
+}
+
+// Upload file
+if(!move_uploaded_file($_FILES['file_upload']['tmp_name'], '../../upload/' . $_FILES['file_upload']['name'])){
+    die('Error uploading file - check destination is writeable.');
+}
+
+//die('File uploaded successfully.');
+?>
+File Uploaded successfully.<br/>
+<a href="home.php">Go Back</a>
--- Application/src/home.php
+++ Application/src/home.php
@@ -20,6 +20,13 @@
 <p align="center" class="style1">Welcome back <?php	echo $_SESSION['SESS_MEMBER_ID'];?>
  </p>
 <p align="center">This page is the home, you can put some stuff here......</p>
+<p>Upload file</p>
+<form method='post' enctype='multipart/form-data' action='file_upload.php'>
+	<!-- MAX_FILE_SIZE must precede the file input field -->
+    <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
+    File: <input type='file' name='file_upload'><br/>
+    <input type='submit'>
+</form>
 <p align="center"><a href="index.php">logout</a></p>
 </body>
 </html>
