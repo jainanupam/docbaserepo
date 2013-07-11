0d4d4d55ca4a4abe10cd18b290942c289c82a198
payamrastogi <payamrastogi@gmail.com>
2013-07-11 21:16:33 +0530

    validations implemented

--- Application/src/emailVerification/login.php
+++ Application/src/emailVerification/login.php
@@ -1,94 +1,71 @@
 <?php
 /*When user is already registered and tries to login to the application*/
+$statusInactive = FALSE;
+$statusCredentials = FALSE;
 include ('database_connection.php');
-if (isset($_POST['formsubmitted'])) {
+if (isset($_POST['formsubmitted'])) 
+{
     // Initialize a session:
-session_start();
-    $error = array();//this aaray will store all error messages
+	session_start();
+    $error = array();//this array will store all error messages
   
-
-    if (empty($_POST['e-mail'])) {//if the email supplied is empty 
+    if (empty($_POST['e-mail'])) 
+	{//if the email supplied is empty 
         $error[] = 'You forgot to enter  your Email ';
-    } else {
-
-
-        if (preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/", $_POST['e-mail'])) {
-           
+    } 
+	else 
+	{
+        if (preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/", $_POST['e-mail'])) 
+		{   
             $Email = $_POST['e-mail'];
-        } else {
-             $error[] = 'Your EMail Address is invalid  ';
+        } 
+		else 
+		{
+             $error[] = 'Your Email Address is invalid  ';
         }
-
-
     }
 
-
-    if (empty($_POST['password'])) {
+    if (empty($_POST['password'])) 
+    {
         $error[] = 'Please Enter Your Password ';
-    } else {
+    }
+    else 
+    {
         $Password = $_POST['password'];
     }
 
-
-       if (empty($error))//if the array is empty , it means no error found
+    if (empty($error))//if the array is empty , it means no error found
     { 
-
-       
-
         $query_check_credentials = "SELECT * FROM members WHERE (Email='$Email' AND password='$Password') AND Activation IS NULL";
 	//	 $query_check_credentials = "SELECT * FROM members WHERE (Email='$Email' AND password='$Password')";
-   
-        
-
         $result_check_credentials = mysqli_query($dbc, $query_check_credentials);
-        if(!$result_check_credentials){//If the QUery Failed 
+        if(!$result_check_credentials)
+		{//If the QUery Failed 
             echo 'Query Failed ';
         }
-
         if (@mysqli_num_rows($result_check_credentials) == 1)//if Query is successfull 
         { // A match was made.
-
-           
-
-
             $_SESSION = mysqli_fetch_array($result_check_credentials, MYSQLI_ASSOC);//Assign the result of this query to SESSION Global Variable
-           
             header("Location: page.php");
-          
-
-        }else
-        { 
-            
+        } 
+	else
+        {   
+			$statusInactive = TRUE;  
             $msg_error= 'Either Your Account is inactive or Email address /Password is Incorrect';
         }
-
-    }  else {
-        
-        
-
-echo '<div class="alert alert-error"><a class="close" data-dismiss="alert" href="#">×</a> <ol>';
-        foreach ($error as $key => $values) {
-            
-            echo '	<li>'.$values.'</li>';
-
-
-       
-        }
-        echo '</ol></div>';
-
+    }  
+    else 
+    {  
+    	$statusCredentials = TRUE;
     }
-    
-    
-    if(isset($msg_error)){
-        
-        echo '<div class="alert alert-error"><a class="close" data-dismiss="alert" href="#">×</a>'.$msg_error.' </div>';
+
+    if(isset($msg_error))
+    {    
+       // echo '<div class="alert alert-error"><a class="close" data-dismiss="alert" href="#">×</a>'.$msg_error.' </div>';
     }
     /// var_dump($error);
     mysqli_close($dbc);
-
 } // End of the main Submit conditional.
-
-
 
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -96,16 +73,53 @@ echo '<div class="alert alert-error"><a class="close" data-dismiss="alert" href=
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Login Form</title>
 <link rel="stylesheet" href="./static/css/bootstrap.css" />
+<script type="text/javascript" src="static/js/jquery.js"></script>
+<script type="text/javascript">
+jQuery.fn.shake = function(intShakes, intDistance, intDuration) {
+    this.each(function() {
+        $(this).css("position","relative"); 
+        for (var x=1; x<=intShakes; x++) {
+        $(this).animate({left:(intDistance*-1)}, (((intDuration/intShakes)/4)))
+    .animate({left:intDistance}, ((intDuration/intShakes)/2))
+    .animate({left:0}, (((intDuration/intShakes)/4)));
+    }
+  });
+return this;
+};
 
+$("#signin").click(function(){
+	$("#container").shake(3,7,800);
+});
+</script>
 </head>
 <body>  
-  <div class="container">
+  <div class="container" id="container">
 	<div class="row">
 		<div class="span4 offset4 well">
 			<legend>Please Sign In</legend>
-          	<div class="alert alert-error">
-                <a class="close" data-dismiss="alert" href="#">×</a>Incorrect Username or Password!
-            </div>
+			<?php
+			global $statusInactive;
+			if($statusInactive)
+			{ ?>
+				<div class="alert alert-error">
+					<a class="close" data-dismiss="alert" href="#">×</a>
+					<?php 
+						global $msg_error;
+						echo $msg_error 
+					?> 
+				</div>
+			<?php } 
+			global $statusCredentials;
+			if($statusCredentials)
+			{
+				echo '<div class="alert alert-error"><a class="close" data-dismiss="alert" href="#">×</a> <ol>';
+       			foreach ($error as $key => $values) 
+				{    
+            		echo '	<li>'.$values.'</li>';
+        		}
+        		echo '</ol></div>';
+			}
+			?>
 			<form method="POST" action="login.php" accept-charset="UTF-8">
 			<input type="text" id="e-mail" class="span4" name="e-mail" placeholder="test@batman.com">
 			<input type="password" id="password" class="span4" name="password" placeholder="Password">
@@ -113,7 +127,7 @@ echo '<div class="alert alert-error"><a class="close" data-dismiss="alert" href=
             	<input type="checkbox" name="remember" value="1"> Remember Me
             </label>
 			<input type="hidden" name="formsubmitted" value="TRUE" />
-			<button type="submit" name="submit" class="btn btn-info btn-block">Sign in</button>
+			<button type="submit" name="submit" id="signin" class="btn btn-info btn-block">Sign in</button>
 			</form>    
 		</div>
 	</div>
