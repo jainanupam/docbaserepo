506382cf4c13f85190b8450e7072250713b2bfb3
payamrastogi <payamrastogi@gmail.com>
2013-07-11 19:28:27 +0530

    validation messages added

--- Application/src/emailVerification/index.php
+++ Application/src/emailVerification/index.php
@@ -1,56 +1,59 @@
 
 <?php
 /*registration page*/
-
-
 include ('database_connection.php');
 require($_SERVER['DOCUMENT_ROOT'].'emailVerification/class.phpmailer.php');
 require($_SERVER['DOCUMENT_ROOT'].'emailVerification//class.smtp.php');
-if (isset($_POST['formsubmitted'])) {
+if (isset($_POST['formsubmitted'])) 
+{
     $error = array();//Declare An Array to store any error message  
-    if (empty($_POST['name'])) {//if no name has been supplied 
+    if (empty($_POST['name'])) 
+	{//if no name has been supplied 
         $error[] = 'Please Enter a name ';//add to array "error"
-    } else {
+    } 
+	else 
+	{
         $name = $_POST['name'];//else assign it a variable
     }
 
-    if (empty($_POST['e-mail'])) {
+    if (empty($_POST['e-mail'])) 
+	{
         $error[] = 'Please Enter your Email ';
-    } else {
-
-
-        if (preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/", $_POST['e-mail'])) {
+    } 
+	else 
+	{
+        if (preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/", $_POST['e-mail'])) 
+		{
            //regular expression for email validation
             $Email = $_POST['e-mail'];
-        } else {
+        } 
+		else 
+		{
              $error[] = 'Your EMail Address is invalid  ';
         }
-
-
     }
 
-
-    if (empty($_POST['password'])) {
+    if (empty($_POST['password'])) 
+	{
         $error[] = 'Please Enter Your Password ';
-    } else {
+    }
+	else 
+	{
         $Password = $_POST['password'];
     }
 
-
     if (empty($error)) //send to Database if there's no error '
-
     { // If everything's OK...
-
         // Make sure the email address is available:
         $query_verify_email = "SELECT * FROM members  WHERE Email ='$Email'";
         $result_verify_email = mysqli_query($dbc, $query_verify_email);
-        if (!$result_verify_email) {//if the Query Failed ,similar to if($result_verify_email==false)
+        if (!$result_verify_email) 
+		{//if the Query Failed ,similar to if($result_verify_email==false)
             echo ' Database Error Occured ';
         }
 
-        if (mysqli_num_rows($result_verify_email) == 0) { // IF no previous user is using this email .
-
-
+        if (mysqli_num_rows($result_verify_email) == 0) 
+		{ // IF no previous user is using this email .
             // Create a unique  activation code:
             $activation = md5(uniqid(rand(), true));
 
@@ -59,17 +62,15 @@ if (isset($_POST['formsubmitted'])) {
 
 
             $result_insert_user = mysqli_query($dbc, $query_insert_user);
-            if (!$result_insert_user) {
+            if (!$result_insert_user)
+			{
                 echo 'Query Failed ';
             }
-
-            if (mysqli_affected_rows($dbc) == 1) { //If the Insert Query was successfull.
-
-
+            if (mysqli_affected_rows($dbc) == 1) 
+			{ //If the Insert Query was successfull.
                 // Send the email:
                 $message = " To activate your account, please click on this link:  \n\n</br>";
                 $message .= WEBSITE_URL . '/emailVerification/activate.php?email=' . urlencode($Email) . "&key=$activation";
-				
 				$mail  = new PHPMailer();
 				$body=$message;
 				$mail->IsSMTP();
@@ -86,61 +87,48 @@ if (isset($_POST['formsubmitted'])) {
 				$mail->AddAddress($Email,$name);
 				$mail->MsgHTML($body);
 				$mail->IsHTML(true); // send as HTML
+				$result = $mail->Send();
+				$systemError = FALSE;
+				$alreadyRegistered = FALSE;
 				if(!$mail->Send())
 				{
 					echo "Mailer Error: " . $mail->ErrorInfo;
 				}
 				else
 				{
-					echo "Message has been sent";
+					//echo "Message has been sent";
 				}
-				
                 //mail($Email, 'Registration Confirmation', $message, 'From: ismaakeel@gmail.com');
-
                 // Flush the buffered output.
-
-
                 // Finish the page:
-                echo '<div class="success">Thank you for
-registering! A confirmation email
-has been sent to '.$Email.' Please click on the Activation Link to Activate your account </div>';
-
-
-            } else { // If it did not run OK.
-                echo '<div class="errormsgbox">You could not be registered due to a system
-error. We apologize for any
-inconvenience.</div>';
+                //echo '<div class="success">Thank you for registering! A confirmation email has been sent to '.$Email.' Please click on the Activation Link to Activate your account </div>';
+            } 
+			else 
+			{ // If it did not run OK.
+               // echo '<div class="errormsgbox">You could not be registered due to a system error. We apologize for any inconvenience.</div>';
+			   $systemError=TRUE;
             }
-
-        } else { // The email address is not available.
-            echo '<div class="errormsgbox" >That email
-address has already been registered.
-</div>';
+        } 
+		else 
+		{ // The email address is not available.
+           // echo '<div class="errormsgbox" >That email address has already been registered.</div>';
+		   $alreadyRegistered=TRUE;
         }
-
-    } else {//If the "error" array contains error msg , display them
-        
-        
-
-echo '<div class="errormsgbox"> <ol>';
-        foreach ($error as $key => $values) {
-            
+    } 
+	else 
+	{//If the "error" array contains error msg , display them
+		echo '<div class="errormsgbox"> <ol>';
+        foreach ($error as $key => $values) 
+		{    
             echo '	<li>'.$values.'</li>';
-
-
-       
         }
         echo '</ol></div>';
-
     }
-  
     mysqli_close($dbc);//Close the DB Connection
-
 } // End of the main Submit conditional.
+?>
 
-
-
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -191,12 +179,34 @@ echo '<div class="errormsgbox"> <ol>';
                                         <button type="submit" class="btn btn-success">Sign Up</button>
 										<button type="button" class="btn">Help</button>
                                     </div>
-                                </div>	
-                                <div class="alert alert-info">
+                                </div>
+								
+								<?php	
+								global $result;
+								if($result)
+								{?>
+									<div id="confirmationAltert" class="alert alert-info">
                                     <button type="button" class="close" data-dismiss="alert">×</button>
                                     <strong>Confirmation: </strong> A confirmation email has been sent to your email.<br>
                                     Thank you for your registration.
                                 </div>
+								<?php 
+								}
+								global $systemError;
+								if($systemError)
+								{?>
+									<div id="systemErrorAltert" class="alert alert-info">
+                                    <button type="button" class="close" data-dismiss="alert">×</button>
+                                    <strong>System Error: </strong>You could not be registered due to a system error. We apologize for any inconvenience.<br>
+						<?php   }
+								global $alreadyRegistered;
+								if($alreadyRegistered)
+								{ ?>
+									<div id="systemErrorAltert" class="alert alert-info">
+                                    <button type="button" class="close" data-dismiss="alert">×</button>
+                                    <strong>System Error: </strong>That email address has already been registered.<br>								
+								<?php 
+								} ?>	
                             </form>	
 						</div>                            
                     </div>
