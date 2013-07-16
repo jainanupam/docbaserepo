09451ca5e6fdf71d9d1e168481f7232d7ca45f5f
payamrastogi <payamrastogi@gmail.com>
2013-07-17 00:09:08 +0530

    new improved login page

--- Application/src/index.php
+++ Application/src/index.php
@@ -3,6 +3,39 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" href="./css/bootstrap.css" />
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+ <style type="text/css">
+      body {
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: #f5f5f5;
+      }
+
+      .form-signin {
+        max-width: 300px;
+        padding: 19px 29px 29px;
+        margin: 0 auto 20px;
+        background-color: #fff;
+        border: 1px solid #e5e5e5;
+        -webkit-border-radius: 5px;
+           -moz-border-radius: 5px;
+                border-radius: 5px;
+        -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+           -moz-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+                box-shadow: 0 1px 2px rgba(0,0,0,.05);
+      }
+      .form-signin .form-signin-heading,
+      .form-signin .checkbox {
+        margin-bottom: 10px;
+      }
+      .form-signin input[type="text"],
+      .form-signin input[type="password"] {
+        font-size: 16px;
+        height: auto;
+        margin-bottom: 15px;
+        padding: 7px 9px;
+      }
+
+    </style>
 <script>
 $(document).ready(function(){
   $(".close").click(function(){
@@ -12,46 +45,33 @@ $(document).ready(function(){
 </script>
 </head>
 <body>
-<div class="container">
-<div class="row">
-<div class="span4 offset4 well">
-			<legend>Sign in</legend>
-<form class="" name="loginform" action="login_exec.php" method="post">
-		<!--the code bellow is used to display the message of the input validation-->
-		 <?php
-			session_start();
-			if( isset($_SESSION['ERRMSG_ARR']) && is_array($_SESSION['ERRMSG_ARR']) && count($_SESSION['ERRMSG_ARR']) >0 )
-			{
-				echo '<div id ="messageBox" class="alert alert-error"><a class="close" data-dismiss="alert" href="#">×</a><ul>';
-				foreach($_SESSION['ERRMSG_ARR'] as $msg) 
+	<div class="container">
+	<?php
+				session_start();
+				if( isset($_SESSION['ERRMSG_ARR']) && is_array($_SESSION['ERRMSG_ARR']) && count($_SESSION['ERRMSG_ARR']) >0 )
 				{
-					echo '<li>',$msg,'</li>'; 
+					echo '<div id ="messageBox" class="alert alert-error"><a class="close" data-dismiss="alert" href="#">×</a><ul>';
+					foreach($_SESSION['ERRMSG_ARR'] as $msg) 
+					{
+						echo '<li>',$msg,'</li>'; 
+					}
+				echo '</ul></div>';
+				unset($_SESSION['ERRMSG_ARR']);
+				unset($_SESSION['SESS_MEMBER_ID']);
 				}
-			echo '</ul></div>';
-			unset($_SESSION['ERRMSG_ARR']);
-			unset($_SESSION['SESS_MEMBER_ID']);
-			}
-			session_destroy();
-		?>
-	<div>
-		<label class="control-label" for="username">Username</label>
-		<input class="span4" id="username" name="username" type="text" tabindex="1" placeholder="test@batman.com" required/>
-	</div>
-    <div>
-		<label class="control-label" for="password">Password</label>
-		<input class="span4" id="password" name="password" type="password" tabindex="2" placeholder="Password" required/>
-	</div>
-  	<div>
-		<label class="checkbox">
-     		<input type="checkbox" name="remember" value="1">Remember Me
-		</label>
-	</div>
-	<div>
-	<button name="submit" type="submit" value="login" tabindex="3" class="btn btn-info btn-block">Sign in</button>
-	</div>
-	</form>
-</div>
-</div>
-</div>
+				session_destroy();
+			?>
+		<form class="form-signin" name="loginform" action="login_exec.php" method="post">
+		<!--the code bellow is used to display the message of the input validation-->
+		 	
+        	<h2 class="form-signin-heading">Please sign in</h2>
+        	<input type="text" id="username" name="username" class="input-block-level" placeholder="Email address" tabindex="1">
+        	<input type="password" id="password" name="password" class="input-block-level" placeholder="Password" tabindex="2">
+        	<label class="checkbox">
+         		<input type="checkbox" value="remember-me"> Remember me
+        	</label>
+        	<button class="btn btn-large btn-primary" type="submit" name="submit" value="login">Sign in</button>
+      </form>
+    </div>
 </body>
 </html>
